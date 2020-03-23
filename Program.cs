4a4bf373ee3bd0898f41f7fdaf16289ae72cc67c
Sean Donker <DonkSean832@GoTVDSB.ca>
2020-03-23 13:47:45 -0400

    All Working

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Part_6_Loops_Handin
         {
             int minValue;
             int maxValue;
-            int randNum;
+            //int randNum;
             int response;
             bool correct = true;
             Random rand1 = new Random();
@@ -25,64 +25,69 @@ namespace Part_6_Loops_Handin
             double percentage = 0;
             bool done = false;
 
-            int minValue2;
-            int maxValue2;
+            int oddSum;
+            int sumOdd = 0;
+
+            int minValue2 = 0;
+            int maxValue2 = 0;
             bool done2 = false;
             bool min = false;
             bool max = false;
+            bool minMaxError = false;
 
-            //Console.WriteLine("Enter a Minimum Value.");
-            //while (!Int32.TryParse(Console.ReadLine(), out minValue))
-            //{
-            //    Console.WriteLine("Error: Enter Correct Type");
-            //}
-            //Console.WriteLine("Enter a Maximum Value.");
-            //while (!Int32.TryParse(Console.ReadLine(), out maxValue))
-            //{
-            //    Console.WriteLine("Error: Enter Correct Type");
+            //Prompter
+            Console.WriteLine("Enter a Minimum Value.");
+            while (!Int32.TryParse(Console.ReadLine(), out minValue))
+            {
+                Console.WriteLine("Error: Enter Correct Type");
+            }
+            Console.WriteLine("Enter a Maximum Value.");
+            while (!Int32.TryParse(Console.ReadLine(), out maxValue))
+            {
+                Console.WriteLine("Error: Enter Correct Type");
 
-            //}
-            //while (maxValue <= minValue) { 
-            //    Console.WriteLine("Error: min cannot be greater than max. Try again.");
-            //    Console.WriteLine("Enter a Minimum Value.");
-            //    while (!Int32.TryParse(Console.ReadLine(), out minValue))
-            //    {
-            //        Console.WriteLine("Error: Enter Correct Type");
-            //    }
-            //    Console.WriteLine("Enter a Maximum Value.");
-            //    while (!Int32.TryParse(Console.ReadLine(), out maxValue))
-            //    {
-            //        Console.WriteLine("Error: Enter Correct Type");
-            //    }
-            //}
-            //Console.Write($"Enter a number between {minValue} and {maxValue}:");
+            }
+            while (maxValue <= minValue)
+            {
+                Console.WriteLine("Error: min cannot be greater than max. Try again.");
+                Console.WriteLine("Enter a Minimum Value.");
+                while (!Int32.TryParse(Console.ReadLine(), out minValue))
+                {
+                    Console.WriteLine("Error: Enter Correct Type");
+                }
+                Console.WriteLine("Enter a Maximum Value.");
+                while (!Int32.TryParse(Console.ReadLine(), out maxValue))
+                {
+                    Console.WriteLine("Error: Enter Correct Type");
+                }
+            }
+            Console.Write($"Enter a number between {minValue} and {maxValue}:");
 
-            //while (correct == true)
-            //{
-            //    if (Int32.TryParse(Console.ReadLine(), out response))
-            //    {
-            //        if (minValue <= response && maxValue >= response)
-            //        {
-            //            Console.WriteLine("");
-            //            Console.WriteLine("Your number is between the range you created");
-            //            correct = false;
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Your number is NOT between the range you created. Try again.");
-            //            Console.WriteLine("");
-            //            Console.Write($"Enter a number between {minValue} and {maxValue}:");
-            //        }
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Error: Enter the correct data type");
-            //        Console.WriteLine("");
-            //        Console.Write($"Enter a number between {minValue} and {maxValue}:");
-            //    }
-            //}
-            //for (int i = 1; i <= 5; i++)
-            //    Console.WriteLine("");
+            while (correct == true)
+            {
+                if (Int32.TryParse(Console.ReadLine(), out response))
+                {
+                    if (minValue <= response && maxValue >= response)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Your number is between the range you created");
+                        correct = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your number is NOT between the range you created. Try again.");
+                        Console.WriteLine("");
+                        Console.Write($"Enter a number between {minValue} and {maxValue}:");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: Enter the correct data type");
+                    Console.WriteLine("");
+                    Console.Write($"Enter a number between {minValue} and {maxValue}:");
+                }
+            }
+            Console.WriteLine("");
 
             //Percent Passing
             Console.WriteLine("Enter your test scores. Enter 'done' to finish entering test scores.");
@@ -114,33 +119,48 @@ namespace Part_6_Loops_Handin
                     else
                         Console.WriteLine("Error: Enter Integer.");    
             }
-
-            //Odd Sum
-            Console.WriteLine("Enter Values to produce a randum number and the sum of all odd numbers to your number.");
-            while(minMaxError)
-            while (!min)
+            //OddSum
+            Console.WriteLine("Enter an integer. This program will print the sum of odd values from 1 to the entered number.");
+            while (!Int32.TryParse(Console.ReadLine(), out oddSum))
             {
-                Console.WriteLine("Enter a minimum value.");
-                if (!Int32.TryParse(Console.ReadLine(), out minValue2))
-                    Console.WriteLine("Error: Enter an Integer");
-                else
-                    min = true;
+                Console.WriteLine("Error: Enter correct data type");
             }
-            while (!max)
+            for (int i = 1; i <= (oddSum + 1); i = i + 2)
+                sumOdd = sumOdd + i;
+            Console.WriteLine($"The sum of odd integers between 1 and {oddSum} is {sumOdd}");
+            Console.WriteLine("");
+
+
+            //Random Numbers
+            Console.WriteLine("Enter Values to recieve 25 values between your chosen range.");
+            while (minMaxError == false)
             {
-                Console.WriteLine("Enter a maximum value.");
-                if (!Int32.TryParse(Console.ReadLine(), out maxValue2))
-                    Console.WriteLine("Error: Enter an Integer");
+                min = false;
+                max = false;
+                while (!min)
+                {
+                    Console.WriteLine("Enter a minimum value.");
+                    if (!Int32.TryParse(Console.ReadLine(), out minValue2))
+                        Console.WriteLine("Error: Enter an Integer");
+                    else
+                        min = true;
+                }
+                while (!max)
+                {
+                    Console.WriteLine("Enter a maximum value.");
+                    if (!Int32.TryParse(Console.ReadLine(), out maxValue2))
+                        Console.WriteLine("Error: Enter an Integer");
+                    else
+                        max = true;
+                }
+                if (maxValue2 >= minValue2)
+                    minMaxError = true;
                 else
-                    max = true;
+                    Console.WriteLine("Error: min cannot be greater than max");
             }
-
-
-
-
-
-
-
+            Console.WriteLine("");
+            for (int i = 0; i <= 25; i++)        
+                Console.WriteLine(rand1.Next(minValue2, maxValue2+1));
             Console.ReadLine();
         }
     }
